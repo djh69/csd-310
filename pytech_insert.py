bdd35f7aed950b9e219344865b61f52804c3df03
djh69 <ponchodamus@gmail.com>
2023-06-17 11:58:31 -0500

    assignment5.1

---
+++ pytech_insert.py
@@ -0,0 +1,26 @@
+MongoDB: insert_one()  
+dillon = {
+ “first_name”: “Dillon”
+}
+ 
+dillon_student_id = students.insert_one(dillon).inserted_id
+ 
+print(dillon_student_id)
+
+MongoDB: insert_one() 
+taylor = {
+ “first_name”: “Taylor”
+}
+ 
+taylor_student_id = students.insert_one(taylor).inserted_id
+ 
+print(taylor_student_id)
+
+MongoDB: insert_one() 
+lexi = {
+ “first_name”: “Lexi”
+}
+ 
+fred_student_id = students.insert_one(Lexi).inserted_id
+ 
+print(Lexi_student_id)
