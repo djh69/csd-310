03e2446913f06827a5bd1c7a6b726f34a08a8c1b
djh69 <134658510+djh69@users.noreply.github.com>
2023-06-21 19:57:52 -0500

    Update mongodb_test.py

--- mongodb_test.py
+++ mongodb_test.py
@@ -1,5 +1,7 @@
-import MongoClient
-url = "mongodb+srv://admin:<password>@cluster0.iicnuuo.mongodb.net/?retryWrites=true&w=majority"
-client = MongoClient(url)
-db = client.pytech
+import pymongo
+from pymongo import MongoClient
+
+client = MongoClient ("mongodb+srv://admin:admin@cluster0.iicnuuo.mongodb.net/?retryWrites=true&w=majority")
+db = client ["pytech"]
+collection = db["students"]
 print(db.list_collection_names)
